7d2b8b7d1b9dea6d4cb0f89508ae8e5bf70515e1
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-10-04 17:29:00 +0500

    last changes

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,5 @@ from .views import get_news_list, get_news_detail
 
 urlpatterns = [
     path('all/', get_news_list, name='get_news_list'),
-    path('<int:pk>/', get_news_detail, name='get_news_detail'),
+    path('detail/<int:pk>/', get_news_detail, name='get_news_detail'),
 ]
